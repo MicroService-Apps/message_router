b39a96f4bb657ea2ce97764dceb14277fcabdf7e
Zhicheng Xu <xzhch1622@gmail.com>
2015-10-24 14:59:26 -0400

    First commit

---
+++ routes/rabbitmqInterface.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib');
+
+var CONTENT_TYPE='application/json';
+var serviceType = 'student';
+
+// Process the received message
+amqp.connect('amqp://localhost').then(function(conn) {
+    process.once('SIGINT', function() { conn.close(); });
+    return conn.createChannel().then(function(ch) {
+        var ok = ch.assertQueue(serviceType, {durable: false});
+        var ok = ok.then(function() {
+            ch.prefetch(1);
+            return ch.consume(serviceType, reply);
+        });
+        return ok.then(function() {
+            console.log(' [x] Awaiting RPC requests');
+        });
+
+        function reply(msg) {
+            var response = {
+                'status': 'good',
+                'message': 'hello'
+            };
+
+            console.log(msg.content.toString());
+            ch.sendToQueue(msg.properties.replyTo,
+                new Buffer(JSON.stringify(response)),
+                {correlationId: msg.properties.correlationId,
+                    contentType:CONTENT_TYPE});
+            ch.ack(msg);
+        }
+    });
+}).then(null, console.warn);
---
+++ routes/studentConnection.js
@@ -0,0 +1,133 @@
+#!/usr/bin/env node
+var amqp = require('amqp');
+var connection = amqp.createConnection({host:'127.0.0.1'});
+
+var rpc = new (require('./amqprpc'))(connection);
+var serviceType = 'student';
+
+exports.execute = function(req, res) {
+    var body = req.body;
+    var method = body.method;
+
+    switch(method) {
+        case 'create':
+            handleCreate(body, res);
+
+            break;
+        case 'read':
+            handleRead(body, res);
+
+            break;
+        case 'update':
+            handleUpdate(body, res);
+
+            break;
+        case 'delete':
+            handleDelete(body, res);
+
+            break;
+        case 'config':
+            handleConfig(body, res);
+
+            break;
+        default:
+            res.send('No such method');
+
+            break;
+    }
+
+
+}
+
+// handle create method
+function handleCreate(body, res) {
+    sendToCourse(body, function response(err, response){
+        if(err) {
+            if(err.code == 'ECONNRESET') {
+                //handle the reponse timeout
+                console.error('response timeout');
+            }
+            else
+                console.error(err);
+        }
+        else {
+            console.log("response", response);
+            res.send(response);
+        }
+    });
+}
+
+// handle read method
+function handleRead(body, res) {
+    sendToCourse(body, function response(err, response){
+        if(err) {
+            if(err.code == 'ECONNRESET') {
+                //handle the reponse timeout
+                console.error('response timeout');
+            }
+            else
+                console.error(err);
+        }
+        else {
+            console.log("response", response);
+            res.send(response);
+        }
+    });
+}
+
+// handle update method
+function handleUpdate(body, res) {
+    sendToCourse(body, function response(err, response){
+        if(err) {
+            if(err.code == 'ECONNRESET') {
+                //handle the reponse timeout
+                console.error('response timeout');
+            }
+            else
+                console.error(err);
+        }
+        else
+            console.log("response", response);
+    });
+}
+
+// handle delete method
+function handleDelete(body, res) {
+    sendToCourse(body, function response(err, response){
+        if(err) {
+            if(err.code == 'ECONNRESET') {
+                //handle the reponse timeout
+                console.error('response timeout');
+            }
+            else
+                console.error(err);
+        }
+        else {
+            console.log("response", response);
+            res.send(response);
+        }
+    });
+}
+
+// handle config method
+function handleConfig(body, res) {
+    sendToCourse(body, function response(err, response){
+        if(err) {
+            if(err.code == 'ECONNRESET') {
+                //handle the reponse timeout
+                console.error('response timeout');
+            }
+            else
+                console.error(err);
+        }
+        else {
+            console.log("response", response);
+            res.send(response);
+        }
+    });
+}
+
+// Send message to course service
+function sendToCourse(message, response) {
+    rpc.makeRequest(serviceType, message, response);
+}
